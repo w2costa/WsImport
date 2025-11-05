5930d816b4fdcda6393cbf8ae61d3e3136cc66fc
Wilson Wolf Costa <wilson.w.costa@gmail.com>
2025-11-05 08:38:17 -0300

    Capítulo 2 - página 42 corrigido

--- demo/src/main/java/com/knight/estoque/Main.java
+++ demo/src/main/java/com/knight/estoque/Main.java
@@ -5,16 +5,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        // Inicia a fábrica dos proxies
-        ListagemLivrosService listagemLivrosFactory = new ListagemLivrosService();
+        // solução do Capítulo 1 página 14
+        // // Inicia a fábrica dos proxies
+        // ListagemLivrosService listagemLivrosFactory = new ListagemLivrosService();
 
-        // Obtém um proxy
-        ListagemLivros listagemLivros = listagemLivrosFactory.getListagemLivrosPort();
-        
+        // // Obtém um proxy
+        // ListagemLivros listagemLivros =
+        // listagemLivrosFactory.getListagemLivrosPort();
+        // // Executa o método remoto
+        // List<Livro> livros = listagemLivros.listarLivros();
+
+        // solução do Capítulo 2 página 42 corrigida
+        ListagemLivrosService listagemLivrosService = new ListagemLivrosService(
+                Livro.class.getResource("/livros.wsdl"));
+        ListagemLivros listagemLivros = listagemLivrosService.getListagemLivrosPort();
         // Executa o método remoto
-        List<Livro> livros = listagemLivros.listarLivros();
+        List<Livro> livros = listagemLivros.listarLivrosPaginacao(0, 2);
+
+        
         for (Livro livro : livros) {
             System.out.println("Nome: " + livro.getNome());
+            System.out.println("Ano da Publicação: " + livro.getAnoDePublicacao());
+            System.out.println("---");
         }
 
     }
